5b1d4f85209e0045c8029483fd7c51d900879d98
Cristian Sierra <cristiansierra.rap@gmail.com>
2025-05-13 23:11:38 -0400

    feat: Create user buttom function added

--- src/app/ui/table/table.component.ts
+++ src/app/ui/table/table.component.ts
@@ -1,14 +1,15 @@
 import {Component} from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import { ServiceService } from './table.service';
-import { Worker } from './table.service';
+import { Worker, CreateWorker } from './table.service';
 import { NgFor } from '@angular/common';
+import { NgForm, FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-table',
   styleUrl: './table.component.css',
   templateUrl: './table.component.html',
   standalone: true,
-  imports: [MatTableModule, NgFor],
+  imports: [MatTableModule, NgFor, FormsModule],
 })
 
 export class TableBasicExample {
@@ -16,6 +17,12 @@ export class TableBasicExample {
   dataSource: Worker[] = []
   displayedColumns: string[] = ['id','fullname', 'phone_number', 'rol'];
   data_query: any;
+  newUser: CreateWorker = {
+    fullname: '',
+    phone_number: '',
+    rol: '',
+    email: ''
+  }
 
   ngOnInit(){
     this.service.getUsers().subscribe({
@@ -25,6 +32,7 @@ export class TableBasicExample {
       }
     })
   }
+
 
   deleteUser(id: string){
     this.service.deleteUser(id).subscribe({
@@ -36,4 +44,18 @@ export class TableBasicExample {
       }
     })
   }
+
+
+  addUser() {
+    this.service.createUser(this.newUser).subscribe({
+      next: (user: Worker) => {
+        this.dataSource.push(user); // o puedes llamar a loadUsers() si prefieres recargar
+        this.newUser = { fullname: '', phone_number: '', rol: '', email: '' }
+      },
+      error: (err) => {
+        console.error('Error al crear usuario:', err);
+      }
+    });
+  }
+
 }
--- src/app/ui/table/table.service.ts
+++ src/app/ui/table/table.service.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface CreateWorker{
+  email: string;
+  fullname: string;
+  phone_number: string;
+  rol: string;
+}
+
 export interface Worker {
   id: string;
   fullname: string;
   phone_number: string;
   rol: string;
+  email: string
 }
 
 @Injectable({
@@ -24,4 +32,7 @@ export class ServiceService {
     return this.http.delete(`http://localhost:8000/worker/${id}`);
   }
 
+  createUser(worker: CreateWorker){
+    return this.http.post<Worker>('http://localhost:8000/worker', worker);
+  }
 }
